b1f56f96e89db5a9b7d83a2528dc4b29f11fb79b
Abedin022 <abedin.niloy.cse@gmail.com>
2025-09-29 00:26:42 +0600

    Added a method file

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import method
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, Sadman!"
+    return method.greet("Sadman")
 
 
 if __name__ == "__main__":
